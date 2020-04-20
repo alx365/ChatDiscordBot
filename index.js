80519b3b5e54d73a49e536e1b21e26229b047438
Alexander Reinhardt <legendofmiracles@protonmail.com>
2020-04-20 19:48:44 +0200

    index:

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-var cp = require('child_process');
-var childProcess = cp.spawn('python', ['-i']);
-const Discord = require('discord.js');
+var cp = require("child_process");
+var childProcess = cp.spawn("python", ["-i"]);
+const Discord = require("discord.js");
 const client = new Discord.Client();
-require('dotenv').config()
+require("dotenv").config();
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+childProcess.stdout.setEncoding("utf8");
 
-childProcess.stdout.setEncoding('utf8')
+var data_line = "";
 
-var data_line = '';
-
-childProcess.stdout.on("data", function(data) {
+childProcess.stdout.on("data", function (data) {
   data_line += data;
-  if (data_line[data_line.length-1] == '\n') {
+  if (data_line[data_line.length - 1] == "\n") {
     // we've got new data (assuming each individual output ends with '\n')
-    var res = data_line
-    data_line = ''; // reset the line of data
+    var res = data_line;
+    data_line = ""; // reset the line of data
 
     console.log(res);
   }
 });
 
-
-childProcess.stdin.write('source activate chatbot\n');
-
-childProcess.stdin.write('python ~/Desktop/Chatbot/chatbot.py\n');
-childProcess.stdin.on("data", function(data) {
-  if(data.toString() == "You:"){
-    childProcess.stdin.write("Hi\n")
+client.on("message", (msg) => {
+  if (msg.content.startsWith("`")) {
+    console.log(msg.content);
+    msg.channel.send("test successful");
   }
-})
+});
 
+childProcess.stdin.write("source activate chatbot\n");
 
+childProcess.stdin.write("python ~/Desktop/Chatbot/chatbot.py\n");
 
 client.login(process.env.token);
