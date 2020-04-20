2c489e8b772e77d066e5e7d4dc259e348d17998f
Alexander Reinhardt <legendofmiracles@protonmail.com>
2020-04-20 17:26:24 +0200

    next steps

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
 var cp = require('child_process');
 var childProcess = cp.spawn('python', ['-i']);
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
 
 childProcess.stdout.setEncoding('utf8')
 
-var k = 0;
 var data_line = '';
 
 childProcess.stdout.on("data", function(data) {
   data_line += data;
   if (data_line[data_line.length-1] == '\n') {
     // we've got new data (assuming each individual output ends with '\n')
-    var res = parseFloat(data_line);
+    var res = data_line
     data_line = ''; // reset the line of data
 
-    console.log('Result #', k, ': ', res);
-
-    k++;
-    // do something else now
-    if (k < 5) {
-      // double the previous result
-      childProcess.stdin.write('2 * + ' + res + '\n');
-    } else {
-      // that's enough
-      childProcess.stdin.end();
-    }
+    console.log(res);
   }
 });
 
 
-childProcess.stdin.write('print("test")\n');
+childProcess.stdin.write('source activate chatbot\n');
 
-/*
-const { spawn } = require('child_process')
-const shell = spawn('sh',[], { stdio: 'inherit' })
-shell.on('close',(code)=>{console.log('[shell] terminated :',code)})
-*/
+childProcess.stdin.write('python ~/Desktop/Chatbot/chatbot.py\n');
+childProcess.stdin.on("data", function(data) {
+  if(data.toString() == "You:"){
+    childProcess.stdin.write("Hi\n")
+  }
+})
