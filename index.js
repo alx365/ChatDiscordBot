ea0b1fb3279aba56da328e4276c1cfcf180a0a90
Alexander Reinhardt <legendofmiracles@protonmail.com>
2020-04-20 20:41:34 +0200

    trying to get child python process to work

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 require("dotenv").config();
 
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setActivity("` um mich zu aktivieren");
@@ -15,7 +16,13 @@ childProcess.stdout.setEncoding("utf8");
 
 var data_line = "";
 
+childProcess.stdin.write("source activate chatbot\n");
+
+childProcess.stdin.write("python ~/Desktop/Chatbot/chatbot.py\n");
+
+
 childProcess.stdout.on("data", function (data) {
+  console.log(data)
   data_line += data;
   if (data_line[data_line.length - 1] == "\n") {
     // we've got new data (assuming each individual output ends with '\n')
@@ -26,17 +33,11 @@ childProcess.stdout.on("data", function (data) {
   }
 });
 
-
-
-childProcess.stdin.write("source activate chatbot\n");
-
-childProcess.stdin.write("python ~/Desktop/Chatbot/chatbot.py\n");
-
 client.on("message", (msg) => {
   if (msg.content.startsWith("`")) {
-    console.log(msg.content);
     var message = msg.content.replace('`', '')
     console.log(message)
+    
   }
 });
 
